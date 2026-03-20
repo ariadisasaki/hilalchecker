6c8415e42dec2be27b0aa90465442c4b9cc23a43
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-03-20 19:02:57 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('hilal-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/style.css',
+        '/js/app.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
