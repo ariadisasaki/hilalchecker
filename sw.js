aad36ecda981afdf82722b420afbf10df46540cb
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-03-21 21:46:19 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,32 @@
-const CACHE_NAME = "hilal-app-v1";
+const CACHE_NAME = "hilal-v1.0.0";
 
 const urlsToCache = [
   "/",
   "/index.html",
   "/css/style.css",
   "/js/app.js",
-  "/assets/icon-192.png"
+  "/assets/icon-192.png",
+  "/assets/icon-512.png"
 ];
 
 self.addEventListener("install", e=>{
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache=>{
       return cache.addAll(urlsToCache);
     })
   );
+});
+
+self.addEventListener("activate", e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>{
+      return Promise.all(
+        keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", e=>{
